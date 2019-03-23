5a980890f884184614508ff8125d3d6cea00cd81
Kari Heikkinen <kari.heikkinen@nordcloud.com>
2019-03-23 18:13:45 +0200

    tslint config

--- src/user/resolvers.ts
+++ src/user/resolvers.ts
@@ -1,10 +1,10 @@
-import { UserProvider } from './provider';
+import { UserProvider, User } from './provider';
 
 export const resolvers = {
   Query: {
-    async me(obj, args, {injector, user}) {
+    async me(obj, args, {injector, user}): Promise<User> {
       const Users: UserProvider = injector.get(UserProvider);
-      return await Users.get(user.id)
+      return await Users.get(user.id);
     }
   }
 };
